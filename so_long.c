10a3fbb73b6838e85f316bc339db58449414062c
gams <gabriel.antonioms@gmail.com>
2021-08-14 20:06:05 -0300

    v.0.4 -close window event with mouse

--- so_long.c
+++ so_long.c
@@ -6,12 +6,13 @@
 /*   By: gantonio <gantonio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/12 22:38:33 by gantonio          #+#    #+#             */
-/*   Updated: 2021/08/14 19:16:06 by gantonio         ###   ########.fr       */
+/*   Updated: 2021/08/14 20:03:15 by gantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include <mlx.h>
+#define ESC 65307
 
 typedef struct	s_data {
 	void	*img;
@@ -25,15 +26,6 @@ typedef struct	s_vars {
 	void	*mlx;
 	void	*win;
 }				t_vars;
-
-int	key_hook(int keycode, t_vars *vars)
-{
-	printf("hey_hook: %c\n", keycode);
-	if (keycode == 'z')	
-		mlx_destroy_window(vars->mlx, vars->win);
-	else if (keycode == 'c')
-		mlx_clear_window(vars->mlx, vars->win);
-}
 
 int draw_line(void *mlx, void *win, int beginX, int beginY, int endX, int endY, int color)
 {
@@ -53,14 +45,29 @@ int draw_line(void *mlx, void *win, int beginX, int beginY, int endX, int endY, 
 	}	
 }
 
+int	key_hook(int keycode, t_vars *vars)
+{
+	printf("hey_hook: %c\n", keycode);
+	if (keycode == ESC)	
+		mlx_destroy_window(vars->mlx, vars->win);
+	else if (keycode == 'c')
+		mlx_clear_window(vars->mlx, vars->win);
+	else
+		draw_line(vars->mlx, vars->win, 640, 360, 0, 0, 0xFFFFFF);
+}
+
+int close_clean(t_vars *vars)
+{
+	mlx_clear_window(vars->mlx, vars->win);
+	mlx_destroy_window(vars->mlx, vars->win);
+}
+
 int	main(void)
 {
 	t_vars	vars;
 	vars.mlx = mlx_init();
     vars.win = mlx_new_window(vars.mlx, 640, 360, "Tutorial Window - Draw Line");
-
-    draw_line(vars.mlx, vars.win, 640, 360, 0, 0, 0xFFFFFF);
-	//mlx_key_hook(win, key_hook, &vars);
 	mlx_hook(vars.win, 2, 1L<<0, key_hook, &vars);
+	mlx_hook(vars.win, 33, 1L<<5, close_clean, &vars);
     mlx_loop(vars.mlx);
 }
--- so_long.h
+++ so_long.h
@@ -1,3 +1,4 @@
 # include "./mlx/mlx.h"
 # include <math.h>
 # include <stdio.h>
+# include <stdlib.h>
