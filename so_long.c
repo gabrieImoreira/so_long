9d8030ceed77606e549433994239e10e6ab5d4a9
gams <gabriel.antonioms@gmail.com>
2021-08-19 19:03:36 -0300

    v.0.3 - add image

--- so_long.c
+++ so_long.c
@@ -6,13 +6,12 @@
 /*   By: gantonio <gantonio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/12 22:38:33 by gantonio          #+#    #+#             */
-/*   Updated: 2021/08/14 20:03:15 by gantonio         ###   ########.fr       */
+/*   Updated: 2021/08/18 20:59:30 by gantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include <mlx.h>
-#define ESC 65307
 
 typedef struct	s_data {
 	void	*img;
@@ -52,8 +51,6 @@ int	key_hook(int keycode, t_vars *vars)
 		mlx_destroy_window(vars->mlx, vars->win);
 	else if (keycode == 'c')
 		mlx_clear_window(vars->mlx, vars->win);
-	else
-		draw_line(vars->mlx, vars->win, 640, 360, 0, 0, 0xFFFFFF);
 }
 
 int close_clean(t_vars *vars)
@@ -65,9 +62,16 @@ int close_clean(t_vars *vars)
 int	main(void)
 {
 	t_vars	vars;
+	t_data	data;
+	char	*relative_path = "./img/wizard.xpm";
+	int		img_width;
+	int		img_height;
 	vars.mlx = mlx_init();
     vars.win = mlx_new_window(vars.mlx, 640, 360, "Tutorial Window - Draw Line");
-	mlx_hook(vars.win, 2, 1L<<0, key_hook, &vars);
-	mlx_hook(vars.win, 33, 1L<<5, close_clean, &vars);
+	data.img = mlx_xpm_file_to_image(vars.mlx, relative_path, &img_width, &img_height);
+	mlx_put_image_to_window(vars.mlx, vars.win, data.img, 100, 100);
+	//mlx_hook(vars.win, 2, 1L<<0, key_hook, &vars);
+	//mlx_hook(vars.win, 33, 1L<<5, close_clean, &vars);
     mlx_loop(vars.mlx);
 }
+
--- so_long.h
+++ so_long.h
@@ -2,3 +2,8 @@
 # include <math.h>
 # include <stdio.h>
 # include <stdlib.h>
+
+# define MLX_SYNC_IMAGE_WRITABLE	1
+# define MLX_SYNC_WIN_FLUSH_CMD		2
+# define MLX_SYNC_WIN_CMD_COMPLETED	3
+# define ESC 65307
